e05fa364fd201da5c1f5dea5ee966ab5e16928d7
Helder de Medeiros Melo <heldermmelo@gmail.com>
2015-10-30 11:08:45 -0300

    limpando o projeto

---
+++ scripts/AeronaveScript.js
@@ -0,0 +1,91 @@
+﻿
+import UnityEngine;
+import Transform;
+
+var isBoat = false;
+var force =0.0;
+var maxSpeed =0.0;
+var turnForce = 0.0;
+var maxTurnSpeed = 0.0;
+var changingAltitudeForce = 0.0;
+var maxChangingAltitudeSpeed=0.0;
+var maxHeight=0.0;
+var maxDepth=0.0;
+var softener=0.0;
+var curSpeed = 0.0;
+var props : Transform[];
+var inputX=0.0;
+var inputY=0.0;
+var inputZ=0.0;
+
+function Start () {
+    GetComponent.<Rigidbody>().maxAngularVelocity=maxTurnSpeed;
+}
+
+function Update () {
+    curSpeed = Mathf.Clamp(GetComponent.<Rigidbody>().velocity.magnitude, 0, maxSpeed);
+    
+    var inputZ=Input.GetAxis("Vertical");
+    var inputX=Input.GetAxis("Horizontal");
+    var inputY=Input.GetAxis("UpAndDown");
+}
+function FixedUpdate(){
+    curSpeed = Mathf.Clamp(GetComponent.<Rigidbody>().velocity.magnitude, 0, maxSpeed);
+    //para trás ou para frente?
+    var inputZ=Input.GetAxis("Vertical");
+    //esquerda ou direita?
+    var inputX=Input.GetAxis("Horizontal");
+    //cima ou baixo?
+    var inputY=Input.GetAxis("UpAndDown");
+    
+    if (inputZ > 0.0 )
+    {
+        GetComponent.<Rigidbody>().AddRelativeForce (0, 0, force);
+    }
+    else if (inputZ < 0.0)
+    {
+        GetComponent.<Rigidbody>().AddRelativeForce (0, 0, -force);
+    }
+    if (inputX > 0.0)
+    {
+        GetComponent.<Rigidbody>().AddRelativeTorque (0, turnForce, 0);
+    }
+    else if (inputX < 0.0)
+    {
+        GetComponent.<Rigidbody>().AddRelativeTorque (0, -turnForce, 0);
+    }
+    if (!isBoat)
+    {
+        if (inputY > 0.0 && GetComponent.<Rigidbody>().velocity.y < maxChangingAltitudeSpeed)
+        {
+            GetComponent.<Rigidbody>().AddRelativeForce (0, changingAltitudeForce, 0);
+        }
+        else if (inputY < 0.0 && GetComponent.<Rigidbody>().velocity.y > (0.0 - maxChangingAltitudeSpeed))
+        {
+            GetComponent.<Rigidbody>().AddRelativeForce (0, -changingAltitudeForce, 0);
+        }
+    }
+    transform.position.y = Mathf.Clamp(transform.position.y, maxDepth, maxHeight);
+    transform.rotation.x = Mathf.Clamp(transform.rotation.x, 0, 0);
+    transform.rotation.z = Mathf.Clamp(transform.rotation.z, 0, 0);
+    //soften are movment
+if(inputX == 0.0)
+GetComponent.<Rigidbody>().angularVelocity *= softener;
+if(inputY == 0.0)
+GetComponent.<Rigidbody>().velocity.y *= softener;
+if(inputZ == 0.0)
+{
+GetComponent.<Rigidbody>().velocity.x *= softener;
+GetComponent.<Rigidbody>().velocity.z *= softener;
+}
+   
+    GetComponent.<Rigidbody>().AddForce(-Vector3.Project(GetComponent.<Rigidbody>().velocity, transform.right) * (GetComponent.<Rigidbody>().mass / 8) );
+}
+   
+  
+
+
+
+    
+   
+
---
+++ scripts/Menu.cs
@@ -0,0 +1,60 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class Menu : MonoBehaviour {
+
+    bool estaVisivel = false;
+
+    // Use this for initialization
+    void Start () {
+
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        if (Input.GetKeyDown(KeyCode.P) && (estaVisivel == false))
+        {
+            estaVisivel = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) && (estaVisivel == true))
+        {
+            estaVisivel = false;
+
+        }
+    }
+    //a classe que vai desenhar interface gráfica
+    Boolean OnGUI() {
+        if (estaVisivel)
+        {
+            if (GUI.Button(new Rect(250, 180, 80, 60), "Novo"))
+            {
+                
+                print("Você apertou o botão");
+                Application.LoadLevel(0);
+
+                
+            }
+            else if (GUI.Button(new Rect(500, 180, 90, 60), "Proximo nível"))
+            {
+                
+                print("Você apertou o botão");
+                Application.LoadLevel(1);
+
+                
+			}
+            
+		}
+        return estaVisivel;
+    }
+      
+    private Rect newRect(int v1, int v2, int v3, int v4, string v5)
+    {
+        throw new NotImplementedException();
+    }
+}
